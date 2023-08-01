5581eabe1a022f59c778ce6109ebf345a44448cd
Guilherme Souza <guilherme.souza@zoop.com.br>
2023-08-01 00:51:34 -0300

    add consumer and producer

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"sync"
 
 	"github.com/jgsouzadev/pfa-go/internal/order/infra/database"
 	"github.com/jgsouzadev/pfa-go/internal/order/usecase"
+	"github.com/jgsouzadev/pfa-go/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	wg := sync.WaitGroup{}
+	maxWorkers := 20
 	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
 
 	if err != nil {
@@ -17,15 +24,41 @@ func main() {
 
 	repository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPriceUseCase(repository)
-	input := usecase.OrderInputDTO{
-		ID:    "1234",
-		Price: 100.0,
-		Tax:   10.0,
-	}
-	output, err := uc.Execute(input)
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
+	defer ch.Close()
+	out := make(chan amqp.Delivery)
 
-	println(output.FinalPrice)
+	go rabbitmq.Consume(ch, out)
+	wg.Add(maxWorkers)
+	for i := 0; 1 < maxWorkers; i++ {
+		defer wg.Done()
+		go worker(out, uc, i)
+	}
+	wg.Wait()
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerId int) {
+	for msg := range deliveryMessage {
+		var input usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &input)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		input.Tax = 10.0
+		_, err = uc.Execute(input)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		//println("WorkerId=", workerId, ", processed order=", input.ID)
+		msg.Ack(false)
+
+	}
 }
